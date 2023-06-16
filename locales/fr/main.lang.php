6b9763917b8a981f0e254333383134ff3ef105e4
franck <carnet.franck.paul@gmail.com>
2023-06-16 13:47:59 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -11,13 +11,13 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Message'] = '';
-L10n::$locales['Subject'] = '';
-L10n::$locales['This category\'s comments RSS feed'] = '';
-L10n::$locales['This category\'s entries RSS feed'] = '';
-L10n::$locales['This page\'s comments RSS feed'] = '';
-L10n::$locales['This post\'s comments RSS feed'] = '';
-L10n::$locales['This tag\'s comments RSS feed'] = '';
-L10n::$locales['This tag\'s entries RSS feed'] = '';
+L10n::$locales['Message'] = 'Message';
+L10n::$locales['Subject'] = 'Sujet';
+L10n::$locales['This category\'s comments RSS feed'] = 'Flux RSS des commentaires de cette catégorie';
+L10n::$locales['This category\'s entries RSS feed'] = 'Flux RSS des billets de cette catégorie';
+L10n::$locales['This page\'s comments RSS feed'] = 'Flux RSS des commentaires de cette page';
+L10n::$locales['This post\'s comments RSS feed'] = 'Flux RSS des commentaires de ce billet';
+L10n::$locales['This tag\'s comments RSS feed'] = 'Flux RSS des commentaires de ce mot-clé';
+L10n::$locales['This tag\'s entries RSS feed'] = 'Flux RSS des billets de ce mot-clé';
 L10n::$locales['next entry'] = 'billet plus r&eacute;cent';
 L10n::$locales['previous entry'] = 'billet plus ancien';
