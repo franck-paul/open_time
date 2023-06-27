9f83e1e7f1f323628fcae4ce29317bf9681d76ab
franck <carnet.franck.paul@gmail.com>
2023-06-27 15:20:26 +0200

    Install/Uninstall

---
+++ src/Install.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @brief open_time, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
+declare(strict_types=1);
+
+namespace Dotclear\Plugin\open_time;
+
+use dcCore;
+use dcNsProcess;
+use Exception;
+
+class Install extends dcNsProcess
+{
+    protected static $init = false; /** @deprecated since 2.27 */
+    public static function init(): bool
+    {
+        static::$init = My::checkContext(My::INSTALL);
+
+        return static::$init;
+    }
+
+    public static function process(): bool
+    {
+        if (!static::$init) {
+            return false;
+        }
+
+        try {
+            // Update
+            $old_version = dcCore::app()->getVersion(My::id());
+            if (version_compare((string) $old_version, '13.42', '<')) {
+                // Do some stuff
+            }
+
+            // Init
+        } catch (Exception $e) {
+            dcCore::app()->error->add($e->getMessage());
+        }
+
+        return true;
+    }
+}
