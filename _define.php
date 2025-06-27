4f11b8a88e18fd34eff683e2abe4eba5caabc3f2
franck <carnet.franck.paul@gmail.com>
2025-06-27 13:21:52 +0200

    Use url(index.php?tf=...) and add overload in define, DC 2.35+

--- _define.php
+++ _define.php
@@ -16,8 +16,9 @@ $this->registerModule(
     '5.3',
     [
         'date'     => '2025-05-15T18:46:59+0200',
-        'requires' => [['core', '2.28']],
+        'requires' => [['core', '2.35']],
         'type'     => 'theme',
+        'overload' => true,
 
         'details'    => 'https://open-time.net/?q=open_time',
         'support'    => 'https://github.com/franck-paul/open_time',
