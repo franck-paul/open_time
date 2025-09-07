227f88488bb43796fb7379e1e0ffa617f6e32d9d
franck <carnet.franck.paul@gmail.com>
2025-09-07 14:20:44 +0200

    New release 7.0

--- _define.php
+++ _define.php
@@ -13,9 +13,9 @@ $this->registerModule(
     'Open-Time',
     'Deviation Cafe-Creme, Structure Aorakit-1B de Kozlika',
     'Franck Paul',
-    '6.1',
+    '7.0',
     [
-        'date'     => '2025-06-30T10:25:15+0200',
+        'date'     => '2025-09-07T14:20:44+0200',
         'requires' => [['core', '2.36']],
         'type'     => 'theme',
         'overload' => true,
